65b226e150c5951346eb2fec5ec5f01336bf6b1b
Elaine Sajets <elainesajets@gmail.com>
2025-02-11 16:44:18 +0900

    Submitting my solution After reviewing the official solution, I want to learn more about array filter(). Looks very sleek. It also seems like I need to read more up on forEach()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+/* const removeFromArray = function (array, num) {
+  const spliced = array.toSpliced(num - 1, 1);
+  return spliced;
+}; */
 
+const removeFromArray = function (array, ...args) {
+  let myArray = array;
+
+  for (let arg of args) {
+    let i = myArray.indexOf(arg);
+
+    while (i !== -1) {
+      if (typeof myArray[i] === typeof arg) {
+        myArray.splice(i, 1);
+      }
+      i = myArray.indexOf(arg);
+    }
+  }
+
+  return myArray;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
