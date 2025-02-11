d425bd4a9d2af91125396d5914035d302059adbe
Elaine Sajets <elainesajets@gmail.com>
2025-02-11 14:41:15 +0900

    Submitting exercise 2 solution Learnt that I can create tests to use later. Tests can be disabled using .skip

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let sentence = '';
+  if (num < 0) {
+    return 'ERROR';
+  }
+  for (let i = 0; i < num; i++) {
+    sentence += string;
+  }
 
+  return sentence;
 };
 
 // Do not edit below this line
