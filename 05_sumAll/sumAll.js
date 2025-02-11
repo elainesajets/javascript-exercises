bf4ccf3af469263318596f3337a52d73ff19b1b2
Elaine Sajets <elainesajets@gmail.com>
2025-02-11 17:36:52 +0900

    Submitting initial solution This works, but i'm worried it will be too complicated as with my previous solutions. I will review the official solution and refactor.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  let sum = 0;
+  let start = Math.min(x, y);
+  let end = Math.max(x, y);
+  const numbers = [];
 
+  function isValid(num) {
+    if (isNaN(num) || !Number.isInteger(num) || num < 0) {
+      return false;
+    }
+    return true;
+  }
+
+  if (!isValid(x) || !isValid(y)) {
+    return 'ERROR';
+  }
+
+  for (let i = start; i <= end; i++) {
+    numbers.push(i);
+  }
+
+  numbers.forEach((num) => (sum += num));
+
+  return sum;
 };
 
 // Do not edit below this line
