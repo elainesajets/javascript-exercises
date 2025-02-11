0a2de09bef4cdf22988e508ed9a86d206270db70
Elaine Sajets <elainesajets@gmail.com>
2025-02-11 18:04:27 +0900

    Refactored the solution after reviewing the official solution I understand how making functions to check validity is not necessary in this case. Simple if statements work better. I also learnt that if there's only one statement in the if statement, curly brackets are not necessary. I also swapped variables using array destructuring which was quite fun! In this way i didn't have to use any of the Math functions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,13 @@
 const sumAll = function (x, y) {
   let sum = 0;
-  let start = Math.min(x, y);
-  let end = Math.max(x, y);
-  const numbers = [];
 
-  function isValid(num) {
-    if (isNaN(num) || !Number.isInteger(num) || num < 0) {
-      return false;
-    }
-    return true;
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+  if (x < 0 || y < 0) return 'ERROR';
+  if (x > y) [x, y] = [y, x]; //Swapping variables with destructuring
+
+  for (let i = x; i <= y; i++) {
+    sum += i;
   }
-
-  if (!isValid(x) || !isValid(y)) {
-    return 'ERROR';
-  }
-
-  for (let i = start; i <= end; i++) {
-    numbers.push(i);
-  }
-
-  numbers.forEach((num) => (sum += num));
 
   return sum;
 };
