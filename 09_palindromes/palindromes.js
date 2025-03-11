cda31feabad18bdadba54398894de8cd6aed2c2f
Elaine Sajets <elainesajets@gmail.com>
2025-03-11 18:39:22 +0900

    Completed exercise. Practiced using a recursive function, however it was not implemented in the final solution. This is because a recursive function would be too slow with large numbers. In this use case, iterating with a for loop is a better solution.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringArr = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '')
+    .split('');
 
+  let reversed = [...stringArr].reverse();
+
+  return stringArr.join('') === reversed.join('');
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function fibonacci(num) {
+  let n = parseInt(num);
 
+  if (n === 1) return 1;
+  if (n === 0) return 0;
+  if (n < 0) return 'OOPS';
+
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 1; i <= n - 1; i++) {
+    let temp = prev + curr;
+    prev = curr;
+    curr = temp;
+  }
+
+  return curr;
+
+  //for loop can be replaced by recursive function:
+  //return fibonacci(n - 1) + fibonacci(n - 2);
+  //however this is not recommended due to it being too slow for large num
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
