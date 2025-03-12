53d56bf01d3cd48e95409c91a9e7a947afa304e8
Elaine Sajets <elainesajets@gmail.com>
2025-03-12 17:05:08 +0900

    Completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function findAge(born, death) {
+  return !death ? new Date().getFullYear() - born : death - born;
+}
 
+const findTheOldest = function (arr) {
+  let sorted = arr.sort(
+    (a, b) =>
+      findAge(b.yearOfBirth, b.yearOfDeath) -
+      findAge(a.yearOfBirth, a.yearOfDeath)
+  );
+
+  return sorted[0];
 };
 
 // Do not edit below this line
